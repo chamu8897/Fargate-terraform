2d276ab080f27a61f64a91b977b5d32e9665864d
chamu <chamukallutla@gmail.com>
2026-02-17 21:51:22 +0530

    creating resource

--- main.tf
+++ main.tf
@@ -1,68 +1,92 @@
 # VPC
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
-# Subnets
+# Public Subnet
 resource "aws_subnet" "public" {
-  count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnets[count.index]
+  cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
+  availability_zone       = "${var.aws_region}a"
+  tags = {
+    Name = "public-subnet"
+  }
 }
 
+# Private Subnet
 resource "aws_subnet" "private" {
-  count      = length(var.private_subnets)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnets[count.index]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "${var.aws_region}b"
+  tags = {
+    Name = "private-subnet"
+  }
 }
 
 # Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
 }
 
 # NAT Gateway
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
 resource "aws_nat_gateway" "natgw" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+  tags = {
+    Name = "main-natgw"
+  }
 }
 
-# Route Tables
-resource "aws_route_table" "public" {
+# Public Route Table
+resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "public-rt"
+  }
 }
 
-resource "aws_route" "public_internet" {
-  route_table_id         = aws_route_table.public.id
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  count          = length(var.public_subnets)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table" "private" {
+# Private Route Table
+resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "private-rt"
+  }
 }
 
-resource "aws_route" "private_nat" {
-  route_table_id         = aws_route_table.private.id
+resource "aws_route" "private_nat_access" {
+  route_table_id         = aws_route_table.private_rt.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.natgw.id
 }
 
 resource "aws_route_table_association" "private_assoc" {
-  count          = length(var.private_subnets)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
 }
 
 # Security Group
--- variable.tf
+++ variable.tf
@@ -1,30 +1,20 @@
-variable "region" {
-  description = "AWS region"
+variable "aws_region" {
+  description = "AWS region to deploy resources"
   type        = string
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
+  description = "CIDR block for the VPC"
   type        = string
 }
 
-variable "public_subnets" {
-  description = "List of public subnet CIDRs"
-  type        = list(string)
-}
-
-variable "private_subnets" {
-  description = "List of private subnet CIDRs"
-  type        = list(string)
-}
-
-variable "state_bucket" {
-  description = "S3 bucket for Terraform state"
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
   type        = string
 }
 
-variable "lock_table" {
-  description = "DynamoDB table for state locking"
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
   type        = string
 }
 

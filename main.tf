22da8fca48a1d36e919a73f423ce8231f895854f
chamu <chamukallutla@gmail.com>
2026-02-17 22:48:55 +0530

    creating resource

--- main.tf
+++ main.tf
@@ -118,6 +118,27 @@ resource "aws_ecr_repository" "repo" {
   name = var.ecr_repo_name
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Effect = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
 # ---------------- ECS Cluster ----------------
 resource "aws_ecs_cluster" "fargate" {
   name = "ecs-fargate-cluster2"
@@ -130,6 +151,7 @@ resource "aws_ecs_task_definition" "app" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
